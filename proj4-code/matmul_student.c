f13d748633c937b4c3bdbbacbf35ba938ef50ba7
SriViswanathan <srilalitheswaran@gmail.com>
2024-12-08 16:36:07 -0600

    Add files via upload

---
+++ proj4-code/matmul_student.c
@@ -0,0 +1,5 @@
+#include "matrix.h"
+
+void matmul_student(const matrix_t *A, const matrix_t *B, matrix_t *C) {
+    // TODO Not Yet Implemented
+}
---
+++ proj4-code/matrix_funcs_student.c
@@ -0,0 +1,10 @@
+#include "matrix.h"
+
+void get_every_fifth_student(const matrix_t *matrix, long results[RESULTS_LEN]) {
+    // TODO Not Yet Implemented
+}
+
+long get_every_student(const matrix_t *matrix) {
+    // TODO Not Yet Implemented
+    return 0;
+}
